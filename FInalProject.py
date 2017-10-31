b10244f752c58fbfe4e436d667a4572d7e6b37f4
stairwaytostability <32941681+stairwaytostability@users.noreply.github.com>
2017-10-31 16:33:25 -0500

    Add files via upload
    This is the overall structure of our code in blocks.  We will be filling the blocks and adding complexity over time.

---
+++ FInalProject.py
@@ -0,0 +1,60 @@
+#! usr/bin/python
+# Code for Final Project 
+
+import sys
+import r
+
+# Connections: Wifi and Bluetooth
+#	BT: Pi needs to receive packets from speaker
+#	
+
+
+
+
+
+
+
+# Alarm System
+#	 initial stage: create a sound at a certain time
+#		- send sound to bluetooth speaker
+#		- complexity: grab song from youtube and send to speaker
+#	 turn the bulb on at a certain time
+#	 	- complexity: gradual brightness and changing color
+
+
+
+
+
+
+
+
+
+# Turning off the alarm
+#	initial stage: press a button
+#		- Complexity: make some game to engage user and turn off alarm when completed successfully
+
+
+
+
+
+
+
+
+
+# GUI
+#	user inputs wake up time
+#		- complexity: choose song and color
+
+
+
+
+
+
+
+
+
+
+# Extras
+#	Play the news after you turn off the alarm
+#		- minute news from NPR
+# 	Top ten youtube songs, pick one using number pad (get rid of keyboard)
